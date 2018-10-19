5306aac0e541687a37c0564412ce6f7119ffcf32
sumedh22 <sumedhchakravorty@yahoo.com>
2018-10-19 16:05:47 +0530

    added table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,17 @@ import { FormComponentComponent } from './components/form-component/form-compone
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../material-component';
 import { RouterModule, Routes } from '@angular/router';
-
+import { TableComponent } from './components/table/table.component';
+import { HttpClientModule,HttpClient } from '@angular/common/http';
 const appRoutes: Routes = [
-  { path: '', component: FormComponentComponent }
+  { path: '', component: FormComponentComponent },
+  {path:'table', component: TableComponent}
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponentComponent
+    FormComponentComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
@@ -26,9 +29,10 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
-    )
+    ),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
